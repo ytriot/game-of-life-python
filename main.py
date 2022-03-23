e2a0a7eb951014da3c3a1d62d29043e6b8f6d7d0
ytriot <tamrub100@gmail.com>
2022-03-23 15:27:52 +0200

    Finish?
    Co-authored-by: Erez Ben Moshe <erezbm@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import itertools
 import os
-from typing import List, Optional
+from time import sleep
+from typing import List, Optional, Generator
 
 
 def main():
-    game = GameOfLife(4, 3)
+    game = GameOfLife(20, 20)
     game.init_state(
-        [1, 1, 0],
-        [0, 0, 1],
+        [0, 1, 0],
         [0, 0, 1],
         [1, 1, 1],
     )
-    game.run()
-
-
-Board = List[List[int]]
-
+    for _ in game.run():
+        clear()
+        print(game)
+        sleep(0.01)
 
 class GameOfLife:
     def __init__(self, rows: int, cols: int) -> None:
@@ -29,27 +28,38 @@ class GameOfLife:
                 self.board[i][j] = cell
 
     def __str__(self) -> str:
-        r = ''
+        r = ' ' + '--' * self.cols + '\n'
         for row in self.board:
-            for cell in row:
-                r += 'X' if cell == 1 else ' '
-            r += os.linesep
+            r += '|' + ''.join('[]' if cell == 1 else '  '
+                               for cell in row) + '|\n'
+        r += ' ' + '--' * self.cols
         return r
 
     def step(self) -> None:
         new_board = [[0] * self.cols for _ in range(self.rows)]
         for i, row in enumerate(self.board):
-            new_board[i] = [self.step_cell(self.board, i, j)
-                            for j, _ in enumerate(row)]
+            new_board[i] = [self._step_cell(i, j) for j, _ in enumerate(row)]
+        self.board = new_board
 
-    @staticmethod
-    def step_cell(board: Board, i: int, j: int) -> bool:
-        return True
+    def _step_cell(self, i: int, j: int) -> int:
+        alive_neighbors = sum(self._neighbors(i, j))
+        return self.board[i][j] if alive_neighbors == 2 else int(alive_neighbors == 3)
 
-    def run(self, steps: Optional[int] = None) -> None:
-        for _ in range(steps) if steps is not None else itertools.count():
+    def _neighbors(self, i: int, j: int) -> List[int]:
+        return [self.board[(i + dy) % self.rows][(j + dx) % self.cols]
+                for dx in [-1, 0, 1]
+                for dy in [-1, 0, 1]
+                if dx != 0 or dy != 0]
+
+    def run(self, steps: Optional[int] = None) -> Generator[int, None, None]:
+        for i in range(steps) if steps is not None else itertools.count():
             self.step()
-            print(self)
+            yield i
+
+
+def clear():
+    # CrOsS- pLaTfOrM ;)
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 if __name__ == '__main__':
